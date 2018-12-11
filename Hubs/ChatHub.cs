08796e7f4447e090b7f8610c3cb8084f009cfa28
Kristijan Klepač <kristijanklepac@Kristijans-iMac.local>
2018-12-11 16:10:07 +0100

    first commit SignalRChat

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿//
+//  Author:
+//    Kristijan Klepač kristijan.klepac@gmail.com
+//
+//  Copyright (c) 2018, www.kristijanklepac.info
+//
+//  All rights reserved.
+//
+//
+using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
